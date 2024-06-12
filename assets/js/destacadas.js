0ac515da7db5213cd33ed8461be74d4a7d2ba350
Martina <scaronemartina@gmail.com>
2024-06-11 23:50:25 -0300

    cambios guardados

---
+++ assets/js/destacadas.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.destacadas-box').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: true
+    });
+});
+
